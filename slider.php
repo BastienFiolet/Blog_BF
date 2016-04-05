4b1f07cd52a18e23c8e160b5d4a8a8ccb9bcc37f
BastienFiolet <fiolet.bastien@gmail.com>
2016-04-05 19:46:37 +0200

    add slide

---
+++ slider.php
@@ -0,0 +1,22 @@
+<?php
+include('includes/header.inc.php');
+include('includes/connexion.inc.php');
+include('includes/verif_util.inc.php');
+?>
+
+<div id="banner">
+
+        <!-- start Basic Jquery Slider -->
+        <ul class="bjqs">
+          <li><a href=""><img src="data/images/banner01.jpg" title="Automatically generated caption"></a></li>
+          <li><img src="data/images/banner02.jpg" title="Automatically generated caption"></li>
+          <li><img src="data/images/banner03.jpg" title="Automatically generated caption"></li>
+        </ul>
+        <!-- end Basic jQuery Slider -->
+</div>
+
+
+
+<?php
+include('includes/footer.inc.php');
+?>
